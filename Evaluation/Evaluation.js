03ef3a95748388a2921a6f868ad46fe8a0d4488f
Ping-jui Li <118146117+louisleedev@users.noreply.github.com>
2023-03-05 15:52:01 -0800

    Bugs fixed
    3/5/2023 Bugs fixed

--- Evaluation/Evaluation.js
+++ Evaluation/Evaluation.js
@@ -6,13 +6,14 @@ const View = (() => {
         remain: document.querySelector("#remain"),
         score: document.querySelector("#score")
     }
-    //create template with array (successful)
+    //Create template with array
     const createTemp = (dataArr) => {
-        let temp = ""
-        const map = new Map()
-        map.set(0, '')
-        map.set(1, '<img src="./mole.jpeg"/>')
-        map.set(2, '<img src="./mine.jpeg"/>')
+        let temp = "";
+        const map = new Map();
+        //Set a map to different objects
+        map.set(0, '');
+        map.set(1, '<img src="./mole.jpeg"/>');
+        map.set(2, '<img src="./mine.jpeg"/>');
         for (let index in dataArr) {
             if (index % 4 == 0) {
                 temp += `<div class="row">
@@ -25,12 +26,12 @@ const View = (() => {
                 temp += `<div class="circle" id="${index}">${map.get(dataArr[index])}</div>`
             }
         }
-        return temp
+        return temp;
     }
 
     const render = (ele, template) => {
         ele.innerHTML = template
-    }
+    };
 
     return {
         domSelector,
@@ -40,35 +41,34 @@ const View = (() => {
 })()
 
 const Model = ((view) => {
-    const { createTemp, render, domSelector } = view
-
+    const { createTemp, render, domSelector } = view;
+    //Record the status of hole_container (Array of 12)
     class State {
         constructor() {
-            this._dataList = []
+            this._dataList = [];
         }
 
         get dataList() {
-            return this._dataList
+            return this._dataList;
         }
 
         set dataList(newList) {
-            this._dataList = newList
-            const temp = createTemp(this._dataList)
-            render(domSelector.hole_container, temp)
+            this._dataList = newList;
+            const temp = createTemp(this._dataList);
+            render(domSelector.hole_container, temp);
         }
     }
+    //Record the score get
     class scoreState {
         constructor() {
-            this._score = 0
+            this._score = 0;
         }
-
         get data() {
-            return this._score
+            return this._score;
         }
-
         set data(newScore) {
-            this._score = newScore
-            render(domSelector.score, `Let's GOOOO!!!!, your total score: ${this._score}`)
+            this._score = newScore;
+            render(domSelector.score, `Let's GOOOO!!!!, your total score: ${this._score}`);
         }
     }
     return {
@@ -80,89 +80,114 @@ const Model = ((view) => {
 })(View)
 
 const Controller = ((view, model) => {
-    const { render, domSelector } = view
-    const { State, scoreState } = model
-    const state = new State()
-    const scorestate = new scoreState()
-    let time = 0
+    const { render, domSelector } = view;
+    const { State, scoreState } = model;
+    const state = new State();
+    const scorestate = new scoreState();
+    let time;
+    let isRunning = false;
+    let activate;
+    //Start a game 
+    const startInterval = () => {
+        let disappearID = [];
+        let lastSnake = [];
+        //Generate moles and mines
+        if (!isRunning) {
+            activate = setInterval(() => {
+                let count = 0;
+                //Change the location of mine every 2 sec
+                if (time % 2 == 0) {
+                    if (lastSnake.length) {
+                        state.dataList[lastSnake.pop()] = 0;
+                        state.dataList = state.dataList;
+                    };
+                    let snake = Math.floor(Math.random() * 12);
+                    while (state.dataList[snake] == 1) {
+                        index = Math.floor(Math.random() * 12);
+                    };
+                    lastSnake.push(snake);
+                    state.dataList[snake] = 2;
+                    state.dataList = state.dataList;
 
-    //hit the mole
+                    if (disappearID.length > 0) {
+                        let toDisappear = disappearID.splice(0, 1);
+                        state.dataList[toDisappear[0]] = 0;
+                        state.dataList = state.dataList;
+                    };
+                };
+                //Count the number of moles
+                for (let i = 0; i < state.dataList.length; i++) {
+                    if (state.dataList[i] === 1) {
+                        count += 1;
+                    };
+                };
+                //Generate a new mole if counts is smaller than 3
+                if (count < 3) {
+                    let index = Math.floor(Math.random() * 12);
+                    while (state.dataList[index] == 1) {
+                        index = Math.floor(Math.random() * 12);
+                    };
+                    state.dataList[index] = 1;
+                    disappearID.push(index);
+                    state.dataList = state.dataList;
+                };
+                //Timer 
+                if (time > 0) {
+                    time -= 1;
+                } else {
+                    stopInterval();
+                    render(domSelector.hole_container, `Your Score is ${scorestate.data}, Restart to Play Again`);
+                };
+                render(domSelector.remain, `Time Left ${time}`);
+            }, 1000);
+            //Record the status of setInterval
+            isRunning = true;
+        };
+    };
+    //Stop the setInterval and change the status to no running
+    const stopInterval = () => {
+        clearInterval(activate);
+        isRunning = false;
+    };
+
+    //Hit the moles and mines
     domSelector.hole_container.addEventListener('click', (event) => {
         let index = event.target.id
         if (state.dataList[index] == 1) {
-            state.dataList[index] = 0
-            state.dataList = state.dataList
-            scorestate.data += 1
+            state.dataList[index] = 0;
+            state.dataList = state.dataList;
+            scorestate.data += 1;
         } else if (state.dataList[index] == 2) {
             const failSnake = new Array(12).fill(2);
-            state.dataList = failSnake
+            state.dataList = failSnake;
+            stopInterval();
         }
     })
 
-    //reset 
+    //Reset and Restart
     domSelector.restart.addEventListener('click', (event) => {
-        time = 30
+        time = 30;
         state.dataList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        scorestate.data = 0
-        let disappearID = []
-        let lastSnake = []
-        //generate a new mole
-        const activate = setInterval(() => {
-            let count = 0
-            if (time % 2 == 0) {
-                if (lastSnake.length) {
-                    state.dataList[lastSnake.pop()] = 0
-                    state.dataList = state.dataList
-                }
-                let snake = Math.floor(Math.random() * 12);
-                while (state.dataList[snake] == 1) {
-                    index = Math.floor(Math.random() * 12);
-                }
-                lastSnake.push(snake)
-                state.dataList[snake] = 2
-                state.dataList = state.dataList
-
-                if (disappearID.length > 0) {
-                    let toDisappear = disappearID.splice(0, 1)
-                    state.dataList[toDisappear[0]] = 0
-                    state.dataList = state.dataList
-                }
-            }
-
-            for (let i = 0; i < state.dataList.length; i++) {
-                if (state.dataList[i] === 1) {
-                    count += 1;
-                }
-            }
-            if (count < 3) {
-                let index = Math.floor(Math.random() * 12);
-                while (state.dataList[index] == 1) {
-                    index = Math.floor(Math.random() * 12);
-                }
-                state.dataList[index] = 1
-                disappearID.push(index)
-                state.dataList = state.dataList
-            }
-            if (time > 0) {
-                time -= 1
-            } else {
-                clearInterval(activate)
-                render(domSelector.hole_container, `Your Score is ${scorestate.data}, Restart to Play Again`)
-            }
-            render(domSelector.remain, `Time Left ${time}`)
-        }, 1000)
-    })
+        scorestate.data = 0;
+        //Decide the function according to the status of setInterval    
+        if (!isRunning) {
+            startInterval();
+        } else {
+            stopInterval();
+            setTimeout(startInterval, 1000);
+        };
+    });
 
     //Get the initail array with no mole
     const bootstrap = () => {
         time = 30;
         state.dataList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        render(domSelector.score, `Let's GOOOO!!!!, your total score: 0`)
-    }
+        render(domSelector.score, `Let's GOOOO!!!!, your total score: 0`);
+    };
     return { bootstrap }
 })(View, Model)
 
-Controller.bootstrap()
+Controller.bootstrap();
 
 
 

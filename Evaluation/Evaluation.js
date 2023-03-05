db5fbc1d3a3eb884932997a12f02317cd11ce987
Ping-jui Li <118146117+louisleedev@users.noreply.github.com>
2023-03-05 14:19:06 -0800

    Advanced feature (Snake) added
    Advanced feature (Snake) added

--- Evaluation/Evaluation.js
+++ Evaluation/Evaluation.js
@@ -1,33 +1,28 @@
-// const Api = (() => {
-
-//     const getInitial = () => {
-//         return fetch("./initial.json").then((res) => res.json())
-//     }
-//     return { getInitial }
-// })()
-
 const View = (() => {
 
     const domSelector = {
         hole_container: document.querySelector("#hole_container"),
         restart: document.querySelector("#restart"),
         remain: document.querySelector("#remain"),
-        score : document.querySelector("#score")
+        score: document.querySelector("#score")
     }
     //create template with array (successful)
     const createTemp = (dataArr) => {
-        console.log(dataArr)
         let temp = ""
+        const map = new Map()
+        map.set(0, '')
+        map.set(1, '<img src="./mole.jpeg"/>')
+        map.set(2, '<img src="./mine.jpeg"/>')
         for (let index in dataArr) {
             if (index % 4 == 0) {
                 temp += `<div class="row">
-                <div class="circle" id="${index}">${dataArr[index] && '<img src="./mole.jpeg"/>'}</div>
+                <div class="circle" id="${index}">${map.get(dataArr[index])}</div>
                 `
             } else if (index % 4 == 3) {
-                temp += `<div class="circle" id="${index}">${dataArr[index] && '<img src="./mole.jpeg"/>'}</div>
+                temp += `<div class="circle" id="${index}">${map.get(dataArr[index])}</div>
                 </div>`
             } else {
-                temp += `<div class="circle" id="${index}">${dataArr[index] && '<img src="./mole.jpeg"/>'}</div>`
+                temp += `<div class="circle" id="${index}">${map.get(dataArr[index])}</div>`
             }
         }
         return temp
@@ -35,7 +30,6 @@ const View = (() => {
 
     const render = (ele, template) => {
         ele.innerHTML = template
-        console.log("success")
     }
 
     return {
@@ -87,8 +81,7 @@ const Model = ((view) => {
 
 const Controller = ((view, model) => {
     const { render, domSelector } = view
-    const { State, scoreState} = model
-    // const { getInitial } = api
+    const { State, scoreState } = model
     const state = new State()
     const scorestate = new scoreState()
     let time = 0
@@ -96,11 +89,13 @@ const Controller = ((view, model) => {
     //hit the mole
     domSelector.hole_container.addEventListener('click', (event) => {
         let index = event.target.id
-        if (state.dataList[index] == 1){
-            state.dataList[index] = 0 
-            let newList = state.dataList
-            state.dataList = newList
+        if (state.dataList[index] == 1) {
+            state.dataList[index] = 0
+            state.dataList = state.dataList
             scorestate.data += 1
+        } else if (state.dataList[index] == 2) {
+            const failSnake = new Array(12).fill(2);
+            state.dataList = failSnake
         }
     })
 
@@ -109,56 +104,65 @@ const Controller = ((view, model) => {
         time = 30
         state.dataList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         scorestate.data = 0
-        activate()
-    })
-
-    //generate a new mole
-    const activate = () => setInterval(()=>{
+        let disappearID = []
+        let lastSnake = []
+        //generate a new mole
+        const activate = setInterval(() => {
             let count = 0
+            if (time % 2 == 0) {
+                if (lastSnake.length) {
+                    state.dataList[lastSnake.pop()] = 0
+                    state.dataList = state.dataList
+                }
+                let snake = Math.floor(Math.random() * 12);
+                while (state.dataList[snake] == 1) {
+                    index = Math.floor(Math.random() * 12);
+                }
+                lastSnake.push(snake)
+                state.dataList[snake] = 2
+                state.dataList = state.dataList
+
+                if (disappearID.length > 0) {
+                    let toDisappear = disappearID.splice(0, 1)
+                    state.dataList[toDisappear[0]] = 0
+                    state.dataList = state.dataList
+                }
+            }
+
             for (let i = 0; i < state.dataList.length; i++) {
                 if (state.dataList[i] === 1) {
                     count += 1;
                 }
             }
-            console.log(count)
             if (count < 3) {
                 let index = Math.floor(Math.random() * 12);
                 while (state.dataList[index] == 1) {
                     index = Math.floor(Math.random() * 12);
                 }
-                state.dataList[index] = 1 
-                let newList = state.dataList
-                state.dataList = newList
+                state.dataList[index] = 1
+                disappearID.push(index)
+                state.dataList = state.dataList
             }
-            if (time > 0){
+            if (time > 0) {
                 time -= 1
-            }else{
+            } else {
                 clearInterval(activate)
                 render(domSelector.hole_container, `Your Score is ${scorestate.data}, Restart to Play Again`)
             }
             render(domSelector.remain, `Time Left ${time}`)
-        }, 1000) 
+        }, 1000)
+    })
+
     //Get the initail array with no mole
     const bootstrap = () => {
         time = 30;
         state.dataList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        render(domSelector.score, `Let's GOOOO!!!!, your total score:0`)
+        render(domSelector.score, `Let's GOOOO!!!!, your total score: 0`)
     }
     return { bootstrap }
 })(View, Model)
 
 Controller.bootstrap()
-//Trigger the function of generate moles in Controller
-// const setTime = setInterval(() => {
-//     Controller.setTimer()
-// }, 1000)
-
-
-
-
-// const tmp = View.createTemp([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-// console.log(tmp)
-// View.render(View.domSelector.hole_container, tmp)
 
 
 
